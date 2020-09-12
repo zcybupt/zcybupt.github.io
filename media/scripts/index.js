e50f4952ff4f247de737ae4b9983b7b954ed8d95
zcybupt <jp2016213431@qmul.ac.uk>
2020-09-12 20:23:19 +0800

    update from gridea: 2020-09-12 20:23:19

---
+++ media/scripts/index.js
@@ -0,0 +1,9 @@
+console.log('Hello Gridea')
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8108f369ee3a154c180974606d5973c5";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
